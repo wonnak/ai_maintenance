c3db8e31bee09e931a98f1b46241fb23f378a31c
ilseok <kimilsuk1310@gmail.com>
2025-05-14 14:32:42 +0900

    상용웹어플리케이션에서 테스트 하기 위해 주석처리

--- global-setup.ts
+++ global-setup.ts
@@ -11,12 +11,12 @@ async function globalSetup(config: FullConfig) {
     height: 1000,
   });
   
-  await page.goto(baseURL + 'loginform.sms');
-  await page.locator('#uid').fill('somansa');
-  await page.locator('#upw').fill('Sms980502!');
-  await page.getByRole('button', { name: '로그인' }).click();
-  await page.context().storageState({ path: storageState as string });
-  await browser.close();
+  // await page.goto(baseURL + 'loginform.sms');
+  // await page.locator('#uid').fill('somansa');
+  // await page.locator('#upw').fill('Sms980502!');
+  // await page.getByRole('button', { name: '로그인' }).click();
+  // await page.context().storageState({ path: storageState as string });
+  // await browser.close();
 }
 
 export default globalSetup;
